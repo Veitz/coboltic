79cb24997c81452e3e79187379ad01210bad83a5
Veitzdev <53821621+Veitz@users.noreply.github.com>
2025-05-24 22:53:54 +0200

    Add files via upload

--- NettoBruttoCalc.cob
+++ NettoBruttoCalc.cob
@@ -5,11 +5,13 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77 NETTOPREIS        PIC 9(5)V99.
-       77 MWST              PIC 9(5)V99 VALUE 0.
-       77 BRUTTOPREIS       PIC 9(5)V99 VALUE 0.
+       77 NETTOPREIS        PIC 9(6)V99.
+       77 MWST              PIC 9(6)V99 VALUE 0.
+       77 BRUTTOPREIS       PIC 9(6)V99 VALUE 0.
 
        PROCEDURE DIVISION.
+           DISPLAY "MERKE: Du kannst maximal 6 stellige Zahlenwerte " 
+                   "mit 2 Nachkommastellen verwenden!"
            DISPLAY "Gib den Nettopreis ein (z. B. 100.00): "
            ACCEPT NETTOPREIS
 
